d323515db83b8099e71bb37a177aa5e8978a5b16
pinglu85 <luping8576@hotmail.com>
2021-04-22 21:29:45 +0200

    refactor(VolumeSlider): implement slider with input type range

--- src/ControlPanel/VolumeSlider/index.js
+++ src/ControlPanel/VolumeSlider/index.js
@@ -1,122 +1,35 @@
-import { useRef, useEffect } from 'react';
-
 import styles from './style.module.css';
 
-const SLIDER_WIDTH = 240;
-const HANDLER_RADIUS = 14;
-const HANDLER_RADIUS_SLIDER_RATIO = Math.floor(
-  (HANDLER_RADIUS / SLIDER_WIDTH) * 100
-);
-
 const VolumeSlider = ({ isPowerOn, volume, setVolume, setDisplayContent }) => {
-  const handlerRef = useRef(null);
+  const handleChange = (evt) => {
+    const newVolume = evt.target.value;
 
-  const handleSliderClick = (e) => {
-    const handlerNode = handlerRef.current;
-    if (handlerNode && e.target === handlerNode) {
-      return;
-    }
-
-    const relativeMouseX = getRelativeMousePosX(e.pageX, e.target);
-    const newVolume = computeNewVolume(relativeMouseX);
-    if (isPowerOn) {
-      setVolume(newVolume);
-      setDisplayContent(`Volume: ${newVolume}`);
-    }
+    setVolume(newVolume);
+    setDisplayContent(`Volume: ${newVolume}`);
   };
 
-  useEffect(() => {
-    const handlerNode = handlerRef.current;
-    if (!handlerNode) {
-      return;
-    }
-
-    let isMouseDown = false;
-
-    const handleMouseDown = (e) => {
-      if (e.target === handlerNode) {
-        isMouseDown = true;
-      }
-    };
-
-    const handleMouseMove = (e) => {
-      if (!isMouseDown) {
-        return;
-      }
-      const relativeMouseX = getRelativeMousePosX(
-        e.pageX,
-        handlerNode.parentNode
-      );
-
-      const newVolume = computeNewVolume(relativeMouseX);
-      if (isPowerOn) {
-        setVolume(newVolume);
-        setDisplayContent(`Volume: ${newVolume}`);
-      }
-    };
-
-    const handleMouseUp = () => {
-      isMouseDown = false;
-    };
-
-    document.addEventListener('mousedown', handleMouseDown);
-    document.addEventListener('mousemove', handleMouseMove);
-    document.addEventListener('mouseup', handleMouseUp);
-
-    return () => {
-      document.removeEventListener('mousedown', handleMouseDown);
-      document.removeEventListener('mousemove', handleMouseMove);
-      document.removeEventListener('mouseup', handleMouseUp);
-    };
-  }, [setVolume, setDisplayContent, isPowerOn]);
-
-  const handlerPosX = computeHandlerPosX(volume);
-
   return (
-    <div className={styles.VolumeSlider} onClick={handleSliderClick}>
-      {isPowerOn && (
-        <div
-          className={`${styles.slider} ${styles.currentVolume}`}
-          style={{ width: `${handlerPosX}%` }}
-        ></div>
-      )}
-      <div className={styles.slider}></div>
-      <div
-        ref={handlerRef}
-        className={styles.handler}
-        style={{ left: `${handlerPosX}%` }}
-      ></div>
+    <div className={styles.VolumeSlider}>
+      <input
+        className={styles.slider}
+        type="range"
+        value={volume}
+        min="0"
+        max="100"
+        style={{
+          background: `linear-gradient(
+            to right,
+            #9730ec 0%,
+            #9730ec ${volume}%,
+            rgb(177, 177, 177) ${volume}%,
+            rgb(177, 177, 177) 100%
+          )`,
+        }}
+        disabled={!isPowerOn}
+        onChange={handleChange}
+      />
     </div>
   );
-};
-
-const computeNewVolume = (x) => {
-  const newVolume = Math.floor((x / SLIDER_WIDTH) * 100);
-  if (newVolume > 100) {
-    return 100;
-  } else if (newVolume < 0) {
-    return 0;
-  } else {
-    return newVolume;
-  }
-};
-
-const getRelativeMousePosX = (pageX, slider) => {
-  const sliderRect = slider.getBoundingClientRect();
-  return pageX - sliderRect.left;
-};
-
-const computeHandlerPosX = (volume) => {
-  let handlerPosX = 0;
-  if (volume <= HANDLER_RADIUS_SLIDER_RATIO) {
-    handlerPosX = 0;
-  } else if (volume + HANDLER_RADIUS_SLIDER_RATIO * 2 >= 100) {
-    handlerPosX = 100 - HANDLER_RADIUS_SLIDER_RATIO * 2;
-  } else {
-    handlerPosX = volume - HANDLER_RADIUS_SLIDER_RATIO;
-  }
-
-  return handlerPosX;
 };
 
 export default VolumeSlider;
